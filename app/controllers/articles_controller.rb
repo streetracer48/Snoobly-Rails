10d0a00bc615118c4badc6ad5e618d5ada077d5a
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-02 12:46:15 +0600

    updated article with flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,11 +20,30 @@ class ArticlesController <ApplicationController
         
     end 
 
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+
+        else 
+           render 'edit'
+        end
+    
+    end
+
     def show 
     
         @article = Article.find(params[:id])
 
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    
 
 
     private
