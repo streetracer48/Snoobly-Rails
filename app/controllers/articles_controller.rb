314cb49b33ae2c973c72f826ee5d51c641d96463
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-02 12:03:19 +0600

    adding article and show completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,10 +8,8 @@ class ArticlesController <ApplicationController
 
     def create 
         # render plain:params[:article].inspect
-
         @article = Article.new(article_params)
 
-        
         if @article.save
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -19,9 +17,14 @@ class ArticlesController <ApplicationController
         else 
            render 'new'
         end
-
         
     end 
+
+    def show 
+    
+        @article = Article.find(params[:id])
+
+    end
 
 
     private
