93bb07404f9da3c91a8dd2ea54aa8b176589b513
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-22 01:11:10 +0600

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,16 @@ class UsersController <ApplicationController
     @user = User.find(params[:id])
    end
 
+   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
     def new
 
         @user = User.new
