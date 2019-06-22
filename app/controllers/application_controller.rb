105d87981c49fe0076b26625423daa27e4145044
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-23 03:17:31 +0600

    created some helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method:current_user, :logged_in?
+
+    def current_user
+
+        @current_user || = User.find(session[:user_id]) if session[:user_id]
+
+    end
+
+    def logged_in?
+
+        !!current_user
+    end
+
+    def require_user
+        
+        if !logged_in?
+
+            flash[:danger] = "You need to logged in to get access "
+            redirect_to root_path
+    end
 
     
 end
