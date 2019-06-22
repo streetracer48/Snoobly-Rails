248643d94e752f73cff7c59ab5345345ca9bd8af
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-23 02:08:49 +0600

    login functionality added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController <ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+       if user && user.authenticate(params[:session][:password])
+           session[:user_id] = user.id
 
+           flash[:success] ="Logged in succesfully"
+           redirect_to user_path(user)
+        else 
+            flash.now[:danger] = "there was something wrong with your email or passwor"
+             render 'new'
+        end
     end
-
-
 
 end
