c1bce6a1817e0754c3dd8ab0d16496ee84c7d295
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-06-23 19:46:00 +0600

    set set user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController <ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
     def index
 
         @users = User.all
@@ -7,15 +9,14 @@ class UsersController <ApplicationController
     end
 
    def edit
-    @user = User.find(params[:id])
+  
    end
 
    def show 
-    @user = User.find(params[:id])
+
    end
 
    def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -46,6 +47,10 @@ class UsersController <ApplicationController
         end
 
         private
+
+        def set_user
+          @user = User.find(params[:id])
+        end
 
         def user_params
             params.require(:user).permit(:username, :email, :password)
